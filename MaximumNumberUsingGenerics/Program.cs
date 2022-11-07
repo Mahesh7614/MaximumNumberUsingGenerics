e0b23ef735725369c437a331d65b8bf1d6820fdf
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-07 19:23:47 +0530

    using Floats

--- MaximumNumberUsingGenerics/Program.cs
+++ MaximumNumberUsingGenerics/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumIntegerNumber(20,10,30));
-            Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumfloategerNumber(20,10,30));
+            Console.WriteLine("Maximum Number is : " + MaximumNumber.MaximumfloategerNumber(20.10f,10.50f,30.68f));
         }
     }
 }
