fdfbc665003180d133df21b9e660485488bbd518
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-07 16:07:28 +0530

    Refactor 2

--- MaximumNumberUsingGenerics/Program.cs
+++ MaximumNumberUsingGenerics/Program.cs
@@ -6,11 +6,12 @@ namespace MaximumNumberUsingGenerics
     {
         static void Main(string[] args)
         {
-            MaximumNumber maximum = new MaximumNumber();
-
-            Console.WriteLine("Maximum Interger Number is : " + maximum.MaxNumber<int>(30, 20, 10));
-            Console.WriteLine("Maximum Float Number is : " + maximum.MaxNumber<float>(30, 20, 10));
-            Console.WriteLine("Maximum String Number is : " + maximum.MaxNumber<string>("44", "30", "10"));
+            MaximumNumber<int> maximumInt = new MaximumNumber<int>(30, 20, 10);
+            Console.WriteLine("Maximum Number is : " + maximumInt.MaxMethod());
+            MaximumNumber<float> maximumFloat = new MaximumNumber<float>(10, 20, 30);
+            Console.WriteLine("Maximum Number is : " + maximumFloat.MaxMethod());
+            MaximumNumber<string> maximumString = new MaximumNumber<string>("20","30","10");
+            Console.WriteLine("Maximum Number is : " + maximumString.MaxMethod());
         }
     }
 }
