7b328b1488c09b5ca0c702d6352c4c6da63372fb
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-07 16:52:14 +0530

    Using Max Method

--- MaximumNumberUsingGenerics/Program.cs
+++ MaximumNumberUsingGenerics/Program.cs
@@ -10,8 +10,12 @@ namespace MaximumNumberUsingGenerics
             Console.WriteLine("Maximum Number is : " + maximumInt.MaxMethod());
             MaximumNumber<float> maximumFloat = new MaximumNumber<float>(10, 40, 30, 20);
             Console.WriteLine("Maximum Number is : " + maximumFloat.MaxMethod());
-            MaximumNumber<string> maximumString = new MaximumNumber<string>("20","30","40","10");
+            MaximumNumber<string> maximumString = new MaximumNumber<string>("20", "30", "40", "10");
             Console.WriteLine("Maximum Number is : " + maximumString.MaxMethod());
+
+            int[] arr = { 112, 344, 432, 555, 678 };
+            MaximumNumberUsingMaxMethod<int> maxMethod = new MaximumNumberUsingMaxMethod<int>(arr);
+            maxMethod.PrintMaxValue();
         }
     }
 }
