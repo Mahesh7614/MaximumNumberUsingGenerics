9f6642012a7a825d0754c68b826e3eb2ec94ee58
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-07 12:37:08 +0530

    Max String At Position 1

--- MaximumNumberUsingGenerics/Program.cs
+++ MaximumNumberUsingGenerics/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Maximum String is : " + MaximumNumber.MaximumstringegerNumber("Apple","Peach","Banana"));
+            Console.WriteLine("Maximum String is : " + MaximumNumber.MaximumstringegerNumber("Peach", "Apple","Banana"));
         }
     }
 }
