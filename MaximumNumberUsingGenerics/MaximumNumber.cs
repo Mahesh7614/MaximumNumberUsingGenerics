e5d7cfd2839ffcc8d61bd666c8a5366ebd27318f
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-07 12:21:59 +0530

    Using String

--- MaximumNumberUsingGenerics/MaximumNumber.cs
+++ MaximumNumberUsingGenerics/MaximumNumber.cs
@@ -8,27 +8,27 @@ namespace MaximumNumberUsingGenerics
 {
     public static class MaximumNumber
     {
-        public static float MaximumfloategerNumber(float Number1, float Number2, float Number3)
+        public static string MaximumstringegerNumber(string String1, string String2, string String3)
         {
-            if (Number1.CompareTo(Number2) > 0 && Number1.CompareTo(Number3) > 0 ||
-                Number1.CompareTo(Number2) >= 0 && Number1.CompareTo(Number3) > 0 ||
-                Number1.CompareTo(Number2) > 0 && Number1.CompareTo(Number3) >= 0)
+            if (String1.CompareTo(String2) > 0 && String1.CompareTo(String3) > 0 ||
+                String1.CompareTo(String2) >= 0 && String1.CompareTo(String3) > 0 ||
+                String1.CompareTo(String2) > 0 && String1.CompareTo(String3) >= 0)
             {
-                return Number1;
+                return String1;
             }
-            if (Number2.CompareTo(Number1) > 0 && Number2.CompareTo(Number3) > 0 ||
-                Number2.CompareTo(Number1) >= 0 && Number2.CompareTo(Number3) > 0 ||
-                Number2.CompareTo(Number1) > 0 && Number2.CompareTo(Number3) >= 0)
+            if (String2.CompareTo(String1) > 0 && String2.CompareTo(String3) > 0 ||
+                String2.CompareTo(String1) >= 0 && String2.CompareTo(String3) > 0 ||
+                String2.CompareTo(String1) > 0 && String2.CompareTo(String3) >= 0)
             {
-                return Number2;
+                return String2;
             }
-            if (Number3.CompareTo(Number1) > 0 && Number3.CompareTo(Number2) > 0 ||
-                Number3.CompareTo(Number1) >= 0 && Number3.CompareTo(Number2) > 0 ||
-                Number3.CompareTo(Number1) > 0 && Number3.CompareTo(Number2) >= 0)
+            if (String3.CompareTo(String1) > 0 && String3.CompareTo(String2) > 0 ||
+                String3.CompareTo(String1) >= 0 && String3.CompareTo(String2) > 0 ||
+                String3.CompareTo(String1) > 0 && String3.CompareTo(String2) >= 0)
             {
-                return Number3;
+                return String3;
             }
-            return Number1;
+            return String1;
         }
     }
 }
