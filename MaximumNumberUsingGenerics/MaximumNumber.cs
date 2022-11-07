5b66b85d89d119294a1bfc6816bd020576aa9c7a
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-07 14:56:33 +0530

    Using Float

--- MaximumNumberUsingGenerics/MaximumNumber.cs
+++ MaximumNumberUsingGenerics/MaximumNumber.cs
@@ -8,6 +8,28 @@ namespace MaximumNumberUsingGenerics
 {
     public static class MaximumNumber
     {
+        public static int MaximumIntegerNumber(int Number1, int Number2, int Number3)
+        {
+            if (Number1.CompareTo(Number2) > 0 && Number1.CompareTo(Number3) > 0 ||
+                Number1.CompareTo(Number2) >= 0 && Number1.CompareTo(Number3) > 0 ||
+                Number1.CompareTo(Number2) > 0 && Number1.CompareTo(Number3) >= 0)
+            {
+                return Number1;
+            }
+            if (Number2.CompareTo(Number1) > 0 && Number2.CompareTo(Number3) > 0 ||
+                Number2.CompareTo(Number1) >= 0 && Number2.CompareTo(Number3) > 0 ||
+                Number2.CompareTo(Number1) > 0 && Number2.CompareTo(Number3) >= 0)
+            {
+                return Number2;
+            }
+            if (Number3.CompareTo(Number1) > 0 && Number3.CompareTo(Number2) > 0 ||
+                Number3.CompareTo(Number1) >= 0 && Number3.CompareTo(Number2) > 0 ||
+                Number3.CompareTo(Number1) > 0 && Number3.CompareTo(Number2) >= 0)
+            {
+                return Number3;
+            }
+            return Number3;
+        }
         public static float MaximumfloategerNumber(float Number1, float Number2, float Number3)
         {
             if (Number1.CompareTo(Number2) > 0 && Number1.CompareTo(Number3) > 0 ||
